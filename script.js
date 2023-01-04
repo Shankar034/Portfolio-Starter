25dfece39050ce73f3559eb422dcedf6deb3092f
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-01-04 20:25:12 +0545

    Intro

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 console.log("This is script for css")
 
 
+document.querySelector('.cross').style.display = 'none';
+
+document.querySelector('.hamburger').addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo')
+})
